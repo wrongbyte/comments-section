a8dd2be1d84ca57e6db75cd4cb4c31070f0c219a
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2022-01-09 12:49:25 -0300

    replies rendering and layout

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -2,42 +2,64 @@ import { useState } from 'react';
 import './comment.css';
 // Vamos usar os states do React para funcionalidades como: atualizar comentário, adicionar resposta, etc
 export default function Comment ({
-    key,
     comment,
     image,
     username,
     timeSince,
     score,
+    replies,
     updateScore,
     updateComment,
     setDeleteComment
 }) {
     const [newReply, setNewReply] = useState(false);
     const [edit, setEdit] = useState(false);
-    console.log(username);
     return (
-        <div className='comment'>
-            
-            <div className='scoreColumn'>
-                <img className="flex-item"src='./images/icon-plus.svg'/>
-                <span className="flex-item">{score}</span>
-                <img className="flex-item" src='./images/icon-minus.svg'/>
-            </div>
+        <>
+            <div className='comment'>
 
-            <div className='contentColumn'>
-                <div className='commentHeader'>
-                    <img className='avatar' src={image} alt='avatar'/>
-                    <div className='username'>{username}</div>
-                    <div className='timestamp'>{timeSince}</div>
-                    <div className='replyButton'>
-                        {/* Note: we are using span and image, two inline elements, to make sure they align side-by-side */}
-                    <img src='./images/icon-reply.svg' alt='reply'></img>
-                    <span> Reply</span> 
-                    </div>
+                <div className='scoreColumn'>
+                    <img className="flex-item"src='./images/icon-plus.svg'/>
+                    <span className="flex-item">{score}</span>
+                    <img className="flex-item" src='./images/icon-minus.svg'/>
                 </div>
-                <div className='commentContent'>{comment}</div>
+
+                <div className='contentColumn'>
+                    <div className='commentHeader'>
+                        <img className='avatar' src={image} alt='avatar'/>
+                        <div className='username'>{username}</div>
+                        <div className='timestamp'>{timeSince}</div>
+                        <div className='replyButton'>
+                            {/* Note: we are using span and image, two inline elements, to make sure they align side-by-side */}
+                        <img src='./images/icon-reply.svg' alt='reply'></img>
+                        <span> Reply</span> 
+                        </div>
+                    </div>
+                    <div className='commentContent'>{comment}</div>
+                </div>
+
             </div>
 
-        </div>
+        {replies?.length > 0 && 
+            replies.map((reply) => {
+                return (
+                    <>
+                        <div className='commentReplies'>
+                        <div className='verticalLine'></div>
+                        <Comment
+                            key={reply.id}
+                            comment={reply.content}
+                            image={reply.user.image.png}
+                            username={reply.user.username}
+                            timeSince={reply.createdAt}
+                            score={reply.score}
+                            replies={reply.replies}
+                        />
+                        </div>
+                    </>
+                )
+            })
+        }
+        </>
     )
 }
